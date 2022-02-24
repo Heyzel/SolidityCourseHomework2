7988bd04fc974a4217aabc41800713db2fd3dfcc
Heyzel <heyzelm7@gmail.com>
2022-02-23 23:48:09 -0400

    homework finished and deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,23 @@
 require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.3",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
+    rinkeby: {
+      allowUnlimitedContractSize: true,
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_URL}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
     hardhat: {
       allowUnlimitedContractSize: true
     },
